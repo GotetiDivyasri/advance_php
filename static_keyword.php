98b2620aae063209a2b08716195a9077c4095d65
DivyaSrinivas <91971172+GotetiDivyasri@users.noreply.github.com>
2025-07-29 17:50:17 +0530

    Create static_keyword.php

---
+++ static_keyword.php
@@ -0,0 +1,16 @@
+<?php
+/*
+. static methods and properties can be accessed without creating an object.
+. static methods can be called directly without needing an object of the class.
+. in addition , $this is not available inside static method.
+. static methods and properties can be considered as escaping from object-oriented programming.
+. the static keyword is used to declare static methods.
+. you can add access modifier before static keyword.
+. syntax:
+class democlass{
+ public static function demostaticmethod(){
+	//remaining code.
+ }	
+}
+*/
+?>
