cad9209777baef17c8d58d33d69d5da6c774a0da
Divya <divyagoteti13@example.com>
2025-09-27 20:26:58 +0530

    testing

--- abstract.php
+++ abstract.php
@@ -1,4 +1,6 @@
 <?php
+echo 'hii';
+die;
 /*
 . abstract class is in parent class which methods are there , those methods only we can use in child class.
 . an abstract class is a class that contains at least one abstract method.
@@ -8,36 +10,44 @@
 . abstract method's visibilty can either be public or protected, but not private.
 . ex : when we are building a car, then we have some things for sure for every car needs to have.
 */
-abstract class Animal {
+abstract class Animal
+{
     public $theme;
-    public function __construct($theme){
+    public function __construct($theme)
+    {
         $this->theme = $theme;
-        echo "This is ". $theme." data"."<br>";
+        echo "This is " . $theme . " data" . "<br>";
     }
     public abstract function makeSound($sound);
     protected abstract function livesPlace();
-    public function sleep(){
-        return "Sleeping"."<br>";
+    public function sleep()
+    {
+        return "Sleeping" . "<br>";
     }
 }
-class Dog extends Animal{
+class Dog extends Animal
+{
     protected $dog_sound = "Bow";
-    public function makeSound($sound){
-        return $this->dog_sound = $sound."<br>";
+    public function makeSound($sound)
+    {
+        return $this->dog_sound = $sound . "<br>";
     }
-    public function livesPlace(){
-        return "Street"."<br>";
+    public function livesPlace()
+    {
+        return "Street" . "<br>";
     }
 }
-class Cat extends Animal{
+class Cat extends Animal
+{
     protected $sound = "Meow";
-    public function makeSound($sound){
-        return $sound."<br>";
+    public function makeSound($sound)
+    {
+        return $sound . "<br>";
     }
-    public function livesPlace(){
-        return "Street"."<br>";
+    public function livesPlace()
+    {
+        return "Street" . "<br>";
     }
-
 }
 $obj1 = new Dog('animals');
 echo $obj1->sleep();
@@ -48,4 +58,3 @@ $obj2 = new Cat('animal');
 echo $obj2->sleep();
 echo $obj2->makeSound('Meoww');
 echo $obj2->livesPlace();
-?>
